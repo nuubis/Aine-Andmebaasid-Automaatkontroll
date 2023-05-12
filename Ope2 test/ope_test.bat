74137efd0e8fd431c16742f827b2f7ab7a12c9ee
Martti <marttikakk@hotmail.com>
2023-05-12 11:49:20 +0300

    vana süsteemi arhiveerimine

--- Ope2 test/ope_test.bat
+++
@@ -1,8 +0,0 @@
-del C:\TEMP\ope.* /F
-del C:\TEMP\kodutoo_check.txt /F
-del C:\TEMP\eksam_check.txt /F
-copy ope.* C:\TEMP /Y
-"C:/Program Files/SQL Anywhere 17/bin64/dbisql.exe" -c "uid=dba;pwd=sql;dbf=C:\temp\ope.db" ope_test.sql
-copy c:\TEMP\tulemus.txt . /Y
-copy c:\TEMP\tulemus.csv . /Y
-
